93420fa686d1e9691b476d3eabc59023b2bbaca9
wangtk <wangtk@yangche51.com>
2017-12-14 10:17:05 +0800

    过期的样式修改

--- src/main/java/nc/liat6/data/reader/impl/XlsReader.java
+++ src/main/java/nc/liat6/data/reader/impl/XlsReader.java
@@ -12,6 +12,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
 
 /**
  * xls文件读取
@@ -68,62 +69,113 @@ public class XlsReader extends AbstractReader{
       String v = "";
       HSSFCell cell = row.getCell(i);
       if(null!=cell){
-        switch(cell.getCellType()){
-          case HSSFCell.CELL_TYPE_NUMERIC:
-            double value = cell.getNumericCellValue();
-            try{
-              short format = cell.getCellStyle().getDataFormat();
-              SimpleDateFormat sdf = null;
-              if(176==format||format==14||format==31||format==57||format==58){
-                sdf = new SimpleDateFormat("yyyy-MM-dd");
-              }else if(format==20||format==32){
-                sdf = new SimpleDateFormat("HH:mm");
+//        switch(cell.getCellType()){
+//          case HSSFCell.CELL_TYPE_NUMERIC:
+//            double value = cell.getNumericCellValue();
+//            try{
+//              short format = cell.getCellStyle().getDataFormat();
+//              SimpleDateFormat sdf = null;
+//              if(176==format||format==14||format==31||format==57||format==58){
+//                sdf = new SimpleDateFormat("yyyy-MM-dd");
+//              }else if(format==20||format==32){
+//                sdf = new SimpleDateFormat("HH:mm");
+//              }
+//              Date date = org.apache.poi.ss.usermodel.DateUtil.getJavaDate(value);
+//              v = sdf.format(date);
+//              int len = v.length();
+//              if(10!=len&&5!=len){
+//                throw new IllegalArgumentException();
+//              }
+//            }catch(Exception e){
+//              v = String.valueOf(value);
+//              if(v.endsWith(".0")){
+//                try{
+//                  Double.parseDouble(v);
+//                  v = v.substring(0,v.lastIndexOf("."));
+//                }catch(Exception ex){}
+//              }
+//            }
+//            break;
+//          case HSSFCell.CELL_TYPE_STRING:
+//            v = (cell.getStringCellValue()+"").trim();
+//            break;
+//          case HSSFCell.CELL_TYPE_BOOLEAN:
+//            v = cell.getBooleanCellValue()+"";
+//            break;
+//          case HSSFCell.CELL_TYPE_FORMULA:
+//            try{
+//              v = String.valueOf(cell.getNumericCellValue());
+//            }catch(IllegalStateException e){
+//              v = String.valueOf(cell.getRichStringCellValue());
+//            }
+//            if(v.endsWith(".0")){
+//              try{
+//                Double.parseDouble(v);
+//                v = v.substring(0,v.lastIndexOf("."));
+//              }catch(Exception e){}
+//            }
+//            break;
+//          case HSSFCell.CELL_TYPE_BLANK:
+//            v = "";
+//            break;
+//          case HSSFCell.CELL_TYPE_ERROR:
+//            v = "";
+//            break;
+//          default:
+//            v = "";
+//            break;
+//        }
+          CellType cellType = cell.getCellTypeEnum();
+          if (CellType.NUMERIC.equals(cellType)) {
+              double value = cell.getNumericCellValue();
+              try {
+                  short format = cell.getCellStyle().getDataFormat();
+                  SimpleDateFormat sdf = null;
+                  if (176 == format || format == 14 || format == 31 || format == 57 || format == 58) {
+                      sdf = new SimpleDateFormat("yyyy-MM-dd");
+                  } else if (format == 20 || format == 32) {
+                      sdf = new SimpleDateFormat("HH:mm");
+                  }
+                  Date date = org.apache.poi.ss.usermodel.DateUtil.getJavaDate(value);
+                  v = sdf.format(date);
+                  int len = v.length();
+                  if (10 != len && 5 != len) {
+                      throw new IllegalArgumentException();
+                  }
+              } catch (Exception e) {
+                  v = String.valueOf(value);
+                  if (v.endsWith(".0")) {
+                      try {
+                          Double.parseDouble(v);
+                          v = v.substring(0, v.lastIndexOf("."));
+                      } catch (Exception ex) {
+                      }
+                  }
               }
-              Date date = org.apache.poi.ss.usermodel.DateUtil.getJavaDate(value);
-              v = sdf.format(date);
-              int len = v.length();
-              if(10!=len&&5!=len){
-                throw new IllegalArgumentException();
+          } else if (CellType.STRING.equals(cellType)) {
+              v = (cell.getStringCellValue() + "").trim();
+          } else if (CellType.BOOLEAN.equals(cellType)) {
+              v = cell.getBooleanCellValue() + "";
+          } else if (CellType.FORMULA.equals(cellType)) {
+              try {
+                  v = String.valueOf(cell.getNumericCellValue());
+              } catch (IllegalStateException e) {
+                  v = String.valueOf(cell.getRichStringCellValue());
               }
-            }catch(Exception e){
-              v = String.valueOf(value);
-              if(v.endsWith(".0")){
-                try{
-                  Double.parseDouble(v);
-                  v = v.substring(0,v.lastIndexOf("."));
-                }catch(Exception ex){}
+              if (v.endsWith(".0")) {
+                  try {
+                      Double.parseDouble(v);
+                      v = v.substring(0, v.lastIndexOf("."));
+                  } catch (Exception e) {
+                  }
               }
-            }
-            break;
-          case HSSFCell.CELL_TYPE_STRING:
-            v = (cell.getStringCellValue()+"").trim();
-            break;
-          case HSSFCell.CELL_TYPE_BOOLEAN:
-            v = cell.getBooleanCellValue()+"";
-            break;
-          case HSSFCell.CELL_TYPE_FORMULA:
-            try{
-              v = String.valueOf(cell.getNumericCellValue());
-            }catch(IllegalStateException e){
-              v = String.valueOf(cell.getRichStringCellValue());
-            }
-            if(v.endsWith(".0")){
-              try{
-                Double.parseDouble(v);
-                v = v.substring(0,v.lastIndexOf("."));
-              }catch(Exception e){}
-            }
-            break;
-          case HSSFCell.CELL_TYPE_BLANK:
-            v = "";
-            break;
-          case HSSFCell.CELL_TYPE_ERROR:
-            v = "";
-            break;
-          default:
-            v = "";
-            break;
-        }
+          } else if (CellType.BLANK.equals(cellType)) {
+              v = "";
+          } else if (CellType.ERROR.equals(cellType)) {
+              v = "";
+          } else {
+              v = "";
+          }
       }
       rs.add(v);
     }
